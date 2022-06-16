51a6265f8b90df9ddcdbd3348745effd589eea11
unknown <iagooliveiraviana@gmail.com>
2022-06-16 13:20:35 -0300

    Adding files

---
+++ FundosImobiliarios/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
+using ClosedXML.Excel;
+using System.IO;
+
+namespace FundosImobiliarios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mPegarInformacoes();
+            this.Close();
+        }
+        private void mPegarInformacoes() {
+            try
+            {
+                string iHtmlPagina = mPageSouce("https://www.fundsexplorer.com.br/ranking");
+                DataTable dtTabela = mHtmlTableToDataTable(iHtmlPagina);
+                string iDataAtual = DateTime.Now.ToString("yyyy-MM-dd");
+                mDataTableToWorksheet(dtTabela, "C:\\Users\\iago\\Downloads\\FUNDOS IMOBILIARIOS\\", $"FII_{iDataAtual}", "xlsx");
+            }
+            catch (Exception ex)
+            {
+                mWriteLog("C:\\Users\\iago\\Downloads\\FUNDOS IMOBILIARIOS\\", "A", $"{DateTime.Now.ToString("yyyy-MM-dd")} - {ex.Message}");
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private string mPageSouce(string pURL) {
+            using (WebClient iWebClient = new WebClient()) {
+                iWebClient.Encoding = Encoding.UTF8;
+                return iWebClient.DownloadString(pURL);
+            }
+        }
+        private DataTable mHtmlTableToDataTable(string pHtml)
+        {
+            DataTable table = new DataTable();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(pHtml);
+            var headers = doc.DocumentNode.SelectNodes("//tr/th");
+            foreach (HtmlNode header in headers)
+                table.Columns.Add(header.InnerText); // create columns from th
+                                                     // select rows with td elements 
+            foreach (var row in doc.DocumentNode.SelectNodes("//tr[td]"))
+                table.Rows.Add(row.SelectNodes("td").Select(td => td.InnerText.Replace(".0",",0").Replace("N/A","0")).ToArray());
+            return table;
+        }
+
+        private void mDataTableToWorksheet(DataTable pDataTable,string pFilePath,string pWorksheetName, string pFileExtension = "xlsx") {
+            XLWorkbook wb = new XLWorkbook();
+            wb.Worksheets.Add(pDataTable, pWorksheetName);
+            wb.SaveAs($"{pFilePath}{pWorksheetName}.{pFileExtension}");        
+        }
+
+        private void mWriteLog(string pLogPath,string pFileName,string pTextToWrite) {
+
+            if (!File.Exists($"{pLogPath}{pFileName}.txt"))
+            {
+                File.Create($"{pLogPath}{pFileName}.txt").Close();             
+            }
+            using (TextWriter writer = new StreamWriter($"{pLogPath}{pFileName}.txt", true))
+            {
+                writer.WriteLine(pTextToWrite);
+            }           
+            
+        }
+
+    }
+    
+}
---
+++ FundosImobiliarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FundosImobiliarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FundosImobiliarios")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d009f473-acef-49bd-b930-e353d0fca9f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
